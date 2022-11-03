d6e4f57242e0599fc68a7b3eb4af7d86efbb3f0d
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-11-02 22:56:14 -0300

    🧪

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 import "./index.scss";
 
-import rewindOff from './assets/svg/rewindOff.svg'
+import cloak from './assets/svg/rewindOff.svg'
+import rewind from './assets/svg/rewindOff.svg'
+import play from 'assets/svg/playOff.svg'
+import forward from 'assets/svg/forwardOff.svg'
 
-let img = "./assets/img/No Role Modelz.jpeg"
+document.querySelector('#app').innerHTML = ` 
+  <div class="divBackgroundImg">  
+    <img id="backgroundImg" src="${cloak}" alt="">  
+  </div> 
 
-document.querySelector('#app').innerHTML = ` <div class="divBackgroundImg"> <img id="backgroundImg" src="${img}" alt="">  </div> <div class="player"> <div class="titleMusic"> <h1 id="title">No Role Modelz</h1> <p id="author"> J. Cole</p> </div> <span class="icon"> <img id="rewind" src="${rewindOff}" alt=""> </span> <span class="icon"> <img id="play" src="assets/svg/playOff.svg" alt=""> </span> <span class="icon"> <img id="forward" src="assets/svg/forwardOff.svg" alt=""> </span> </div> `;
+  <div class="player"> 
+
+    <div class="titleMusic"> 
+
+      <h1 id="title">
+        No Role Modelz  
+      </h1> 
+
+      <p id="author"> 
+        J. Cole
+      </p> 
+
+    </div> 
+
+      <span class="icon"> 
+        <img id="rewind" src="${rewind}" alt=""> 
+      </span> 
+      <span class="icon"> 
+        <img id="play" src="${play}" alt=""> 
+      </span> 
+      <span class="icon"> 
+        <img id="forward" src="${forward}" alt=""> 
+      </span> 
+
+  </div> 
+  `;
+
