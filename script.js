235639087ab1ee0bb455ffe08eaf7a9825e27ab3
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-11-02 22:34:12 -0300

    🧪

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 import "./index.scss";
 
-document.querySelector('#app').innerHTML = ' <div class="divBackgroundImg"> <img id="backgroundImg" src="assets/img/No Role Modelz.jpeg" alt="">  </div> <div class="player"> <div class="titleMusic"> <h1 id="title">No Role Modelz</h1> <p id="author"> J. Cole</p> </div> <span class="icon"> <img id="rewind" src="assets/svg/rewindOff.svg" alt=""> </span> <span class="icon"> <img id="play" src="assets/svg/playOff.svg" alt=""> </span> <span class="icon"> <img id="forward" src="assets/svg/forwardOff.svg" alt=""> </span> </div> ';
+import rewindOff from './assets/svg/rewindOff.svg'
+
+let img = "./assets/img/No Role Modelz.jpeg"
+
+document.querySelector('#app').innerHTML = ` <div class="divBackgroundImg"> <img id="backgroundImg" src="${img}" alt="">  </div> <div class="player"> <div class="titleMusic"> <h1 id="title">No Role Modelz</h1> <p id="author"> J. Cole</p> </div> <span class="icon"> <img id="rewind" src="${rewindOff}" alt=""> </span> <span class="icon"> <img id="play" src="assets/svg/playOff.svg" alt=""> </span> <span class="icon"> <img id="forward" src="assets/svg/forwardOff.svg" alt=""> </span> </div> `;
