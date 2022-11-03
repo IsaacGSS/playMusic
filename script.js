c4ac2704a39665936eb091f732330f16de2adc08
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2022-11-02 21:33:25 -0300

    🎨
    Criação e estilização de um player de musica

---
+++ script.js
@@ -0,0 +1,3 @@
+import "./index.scss";
+
+document.querySelector('#app').innerHTML = ' <div class="divBackgroundImg"> <img id="backgroundImg" src="assets/img/No Role Modelz.jpeg" alt="">  </div> <div class="player"> <div class="titleMusic"> <h1 id="title">No Role Modelz</h1> <p id="author"> J. Cole</p> </div> <span class="icon"> <img id="rewind" src="assets/svg/rewindOff.svg" alt=""> </span> <span class="icon"> <img id="play" src="assets/svg/playOff.svg" alt=""> </span> <span class="icon"> <img id="forward" src="assets/svg/forwardOff.svg" alt=""> </span> </div> ';
